f6d1fb4fc9b270aed92a7b3a1f5b548db0769d2d
butters5789 <butters5789@gmail.com>
2022-08-11 17:21:04 -0600

    modal transition

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,17 @@
   </div>
 
   <div class="container">
-    <transition>
+    <transition name="para">
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
     <button @click="toggleParaVisibility">Toggle Paragraph</button>
   </div>
 
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
+
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
@@ -92,30 +93,30 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.v-enter-from {
+.para-enter-from {
   opacity: 0;
   transform: translateY(-30px);
 }
 
-.v-enter-active {
+.para-enter-active {
   transition: all 0.3s ease-out;
 }
 
-.v-enter-to {
+.para-enter-to {
   opacity: 1;
   transform: translateY(0);
 }
 
-.v-leave-from {
+.para-leave-from {
   opacity: 1;
   transform: translateY(0);
 }
 
-.v-leave-active {
+.para-leave-active {
   transition: all 0.3s ease-in;
 }
 
-.v-leave-to {
+.para-leave-to {
   opacity: 0;
   transform: translateY(30px);
 }
