36655f533ba7a87db177eeb9aee4240a539820b4
butters5789 <butters5789@gmail.com>
2022-08-11 16:53:09 -0600

    using transition wrapper

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
     <div class="block" :class="{ animate: animateBlock }"></div>
     <button @click="startAnimateBlock">Animate</button>
   </div>
+
+  <div class="container">
+    <transition>
+      <p v-if="paraIsVisible">This is only sometimes visible...</p>
+    </transition>
+    <button @click="toggleParaVisibility">Toggle Paragraph</button>
+  </div>
+
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
@@ -18,6 +26,7 @@ export default {
     return {
       animateBlock: false,
       dialogIsVisible: false,
+      paraIsVisible: false,
     };
   },
   methods: {
@@ -26,6 +35,9 @@ export default {
     },
     showDialog() {
       this.dialogIsVisible = true;
+    },
+    toggleParaVisibility() {
+      this.paraIsVisible = !this.paraIsVisible;
     },
     hideDialog() {
       this.dialogIsVisible = false;
@@ -63,7 +75,6 @@ button:active {
   height: 8rem;
   background-color: #290033;
   margin-bottom: 2rem;
-  transition: transform 0.3s ease-out;
 }
 .container {
   max-width: 40rem;
@@ -76,7 +87,50 @@ button:active {
   border: 2px solid #ccc;
   border-radius: 12px;
 }
+
 .animate {
-  transform: translateX(-150px);
+  animation: slide-fade 0.3s ease-out forwards;
+}
+
+.v-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.v-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.v-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+
+.v-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+@keyframes slide-fade {
+  0% {
+    transform: translateX(0) scale(1);
+  }
+
+  70% {
+    transform: translateX(-120px) scale(1.1);
+  }
+
+  100% {
+    transform: translateX(-150px) scale(1);
+  }
 }
 </style>
